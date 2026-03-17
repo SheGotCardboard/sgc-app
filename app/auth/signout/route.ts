dd3918753075b87e9e97dce1047d21fb1fe87399
KLW <kristenlweber@users.noreply.github.com>
2026-03-17 16:49:54 -0500

    feat: fixed routes for logout

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -4,5 +4,15 @@ import { NextResponse } from "next/server";
 export async function POST() {
   const supabase = await createClient();
   await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_SITE_URL!));
+  return NextResponse.redirect(
+    new URL("/", process.env.NEXT_PUBLIC_SITE_URL!)
+  );
+}
+
+export async function GET() {
+  const supabase = await createClient();
+  await supabase.auth.signOut();
+  return NextResponse.redirect(
+    new URL("/", process.env.NEXT_PUBLIC_SITE_URL!)
+  );
 }
